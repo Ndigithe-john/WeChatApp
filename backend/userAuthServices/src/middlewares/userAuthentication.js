ce8fe2750106e47848b83a9187e0e581779711e6
Ndigithe-john <ndigithejohn@gmail.com>
2023-07-06 06:24:38 -0700

    commit

--- backend/userAuthServices/src/middlewares/userAuthentication.js
+++ backend/userAuthServices/src/middlewares/userAuthentication.js
@@ -1,16 +1,1 @@
-const { tokenVerifier } = require("../utils/getUser");
-async function tokenValMiddleware(req, res, next) {
-  let token = req.headers["authorization"].split(" ")[1];
-  try {
-    if (!token)
-      return next({ status: 400, message: "Provide a token to proceed" });
-    let user = await tokenVerifier(token);
-    if (member.roles === "admin") {
-      req.member = member;
-      next();
-    }
-  } catch (error) {
-    next({ status: 401, message: error.message });
-  }
-}
-module.exports = tokenValMiddleware;
+
